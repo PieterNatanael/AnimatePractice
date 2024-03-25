d77a4d053e19081c2dcd85f0d38a896b3203f7f4
PieterNatanael <pieternatanael@icloud.com>
2024-03-25 12:35:43 +0800

    Initial Commit

---
+++ AnimatePractice/AnimatePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatePracticeApp.swift
+//  AnimatePractice
+//
+//  Created by Pieter Yoshua Natanael on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimatePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
