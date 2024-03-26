78b38d03d206d89e144da9993b0407686e6f4c7a
PieterNatanael <pieternatanael@icloud.com>
2024-03-26 23:10:59 +0800

    Initial Commit

---
+++ AnimatePractice/TextAppear.swift
@@ -0,0 +1,18 @@
+//
+//  TextAppear.swift
+//  AnimatePractice
+//
+//  Created by Pieter Yoshua Natanael on 26/03/24.
+//
+
+import SwiftUI
+
+struct TextAppear: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextAppear()
+}
---
+++ AnimatePractice/TextRightToCenter.swift
@@ -0,0 +1,18 @@
+//
+//  TextRightToCenter.swift
+//  AnimatePractice
+//
+//  Created by Pieter Yoshua Natanael on 25/03/24.
+//
+
+import SwiftUI
+
+struct TextRightToCenter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextRightToCenter()
+}
