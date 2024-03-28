447ba9f5b2e2cc2537fc8f5c17e4d1aaef274616
PieterNatanael <pieternatanael@icloud.com>
2024-03-28 19:34:18 +0800

    Initial Commit

---
+++ AnimatePractice/CenterRotateBigger.swift
@@ -0,0 +1,18 @@
+//
+//  CenterRotateBigger.swift
+//  AnimatePractice
+//
+//  Created by Pieter Yoshua Natanael on 28/03/24.
+//
+
+import SwiftUI
+
+struct CenterRotateBigger: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CenterRotateBigger()
+}
