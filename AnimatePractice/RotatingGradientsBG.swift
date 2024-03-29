e72ed6059436c5d8341220d7d6f21083b71ff9b3
PieterNatanael <pieternatanael@icloud.com>
2024-03-29 11:54:27 +0800

    Initial Commit

---
+++ AnimatePractice/RotatingGradientsBG.swift
@@ -0,0 +1,18 @@
+//
+//  RotatingGradientsBG.swift
+//  AnimatePractice
+//
+//  Created by Pieter Yoshua Natanael on 29/03/24.
+//
+
+import SwiftUI
+
+struct RotatingGradientsBG: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatingGradientsBG()
+}
